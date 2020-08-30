a920fda324e5d27d3b64a46da46172315bc4058b
chenlicong <chenlicong@123.com>
2020-08-30 11:08:15 +0800

    urls.py也提交

---
+++ urls.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+tcQtUrl = 'http://qt.gtimg.cn/q='
+DDGroupUrl = 'https://oapi.dingtalk.com/robot/send?access_token=414e08cc157d6229a5361cd0fe38bb5bec7cffc3bbee31cc5458adeecfb73bd1'
+# DDGroupUrl = DDTest = 'https://oapi.dingtalk.com/robot/send?access_token=ade8c4666ba4477a4dad6b4359eb83d6a46c22fe85a8163ace13ada1ff4723b3'
+QyweixinUrl = 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=cb724d9f-2e01-42af-8ff7-507588fbfb08'
